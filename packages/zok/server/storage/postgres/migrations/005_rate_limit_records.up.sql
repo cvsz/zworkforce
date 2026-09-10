daf2e39956a3bd25fc283440c145484e6bf76766
cvsz <seaza@msn.com>
2026-09-10 13:46:46 +0700

    chore(zok): convert package to pinned submodule

--- packages/zok/server/storage/postgres/migrations/005_rate_limit_records.up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE rate_limit_records (
-  key TEXT NOT NULL,
-  requested_at TIMESTAMPTZ NOT NULL DEFAULT now()
-);
-
-CREATE INDEX rate_limit_records_key_requested_idx ON rate_limit_records (key, requested_at);
